f94d1f3851c6887f52bdfc674209f94548f80e4f
HarisaranG <harithiru2018@gmail.com>
2020-10-08 10:06:00 +0530

    Dynamic Urls (Pt.2)

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('customer/<str:pk>/', views.customer),
-    path('product/', views.product)
+    path('', views.home, name="home"),
+    path('customer/<str:pk>/', views.customer, name="customer"),
+    path('product/', views.product, name="product")
 ]
