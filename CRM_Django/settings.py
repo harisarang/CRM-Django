7e054df84164c2986a3bf2a3ef160e162d5e3f40
HarisaranG <harithiru2018@gmail.com>
2020-12-16 12:30:03 +0530

    Changed to sqlite3

--- CRM_Django/settings.py
+++ CRM_Django/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'CRM_Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crm',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'database-2.cybi45ufb57i.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
